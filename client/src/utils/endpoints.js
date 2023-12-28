c7aeafb66898d123aed3c128b31a57b86979f854
Yomna Salah <syomna444@gmail.com>
2023-12-28 17:31:44 +0200

    add mongodb atlas and render deployment

--- client/src/utils/endpoints.js
+++ client/src/utils/endpoints.js
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:3000";
+const baseURL = "https://testtt-z8ao.onrender.com";
 const getColumns = `${baseURL}/columns`;
 const getCards = `${baseURL}/cards`;
 const changeCards = `${baseURL}/change-cards`;
--- server/src/index.js
+++ server/src/index.js
@@ -7,10 +7,43 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
-const ColumnModel = require("../models/Column");
-const CardModel = require("../models/Card");
+const ColumnModel = require("./models/Column");
+const CardModel = require("./models/Card");
 
-mongoose.connect(process.env.DB_CONNECT);
+mongoose.connect(
+  "mongodb+srv://yomna:12345678Yo@cluster23429.ddszajq.mongodb.net/test",
+  {
+    user: "yomna",
+    pass: "12345678Yo",
+  }
+);
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://yomna:12345678Yo@cluster23429.ddszajq.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/columns", async (req, res) => {
   const columns = await ColumnModel.find();
@@ -24,10 +57,10 @@ app.get("/cards", async (req, res) => {
 
 app.post("/change-cards", async (req, res) => {
   const columnId = req.body.columnId;
-    const cardId = req.body.cardId;
-    console.log(`columnId ${columnId} cardId ${cardId}`);
+  const cardId = req.body.cardId;
+  console.log(`columnId ${columnId} cardId ${cardId}`);
 
- await ColumnModel.updateMany(
+  await ColumnModel.updateMany(
     { card_id: cardId },
     { $pull: { card_id: cardId } }
   );
@@ -42,4 +75,4 @@ app.post("/change-cards", async (req, res) => {
   res.status(200).json({ message: "updated!" });
 });
 
-app.listen(3000, () => console.log("Running on PORT 3000!"));
+app.listen(3000, "0.0.0.0", () => console.log("Running on PORT 3000!"));
